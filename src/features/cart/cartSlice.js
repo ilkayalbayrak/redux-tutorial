3f8a45093822c04b78fe90459e285d00c7aa47c7
Ilkay Albayrak <ilkayyalbayrak@gmail.com>
2023-04-13 22:08:21 +0200

    added basic actions

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -10,9 +10,44 @@ const initialState = {
 
 const cartSlice = createSlice({
     name:'cart',
-    initialState
+    initialState,
+    reducers: {
+        // we can mutate the state direcly because immer takes care of it in the background
+        // NOTE: an Action automatically created after the code below
+        clearCart: (state) => {
+            state.cartItems = [];
+        },
+        removeItem: (state, action) => {
+            const itemId = action.payload
+            state.cartItems = state.cartItems.filter((item)=>
+            item.id !== itemId);
+        },
+        increase: (state, {payload}) => {
+            const cartItem = state.cartItems.find((item)=>
+            item.id === payload.id);
+            cartItem.amount = cartItem.amount + 1; 
+        },
+        decrease: (state, {payload}) => {
+            const cartItem = state.cartItems.find((item)=>
+            item.id === payload.id);
+            cartItem.amount = cartItem.amount - 1; 
+        },
+        calculateTotals: (state) => {
+            let amount = 0;
+            let total = 0;
+            state.cartItems.forEach((item)=> {
+                amount += item.amount;
+                total += item.amount * item.price
+            });
+            state.amount = amount;
+            state.total = total;
+        }
+    },
 });
 
-// console.log(cartSlice);
+// console.log(cartSlice); 
+// export  the action
+
+export const{ clearCart, removeItem, increase, decrease, calculateTotals } = cartSlice.actions;
 
 export default cartSlice.reducer;
